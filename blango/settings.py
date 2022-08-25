4594f8905dc4d0a718b4d6901105e8d5eef00fa4
Олег Фролов <oleh.frolov@hneu.net>
2022-08-25 19:32:21 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -106,6 +106,7 @@ class Dev(Configuration):
       "rest_framework",
       "rest_framework.authtoken",
       "drf_yasg",
+      "django_filters",
   ]
   #AllaTH
   SITE_ID = 1
@@ -135,6 +136,12 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
 
   }
   SWAGGER_SETTINGS = {
